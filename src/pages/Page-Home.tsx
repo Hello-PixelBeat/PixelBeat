3b5361371b3f4609bce2aa39c5e79c62c467014c
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2024-01-10 16:51:53 +0900

    ✨ feat: entry 페이지 구현 (#10)

--- src/pages/Page-Home.tsx
+++ src/pages/Page-Home.tsx
@@ -1,4 +1,10 @@
+import { Spinner } from "@/components";
+import useUserInfo from "@/hooks/useUserInfo";
+
 const Home = () => {
+	const { isLoading } = useUserInfo();
+
+	if (isLoading) return <Spinner text={"메인 페이지 불러오는 중..."} />;
 	return <div>Page-Home</div>;
 };
 
