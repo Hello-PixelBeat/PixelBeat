42570e936a54d4838578b820a130f42f028a093f
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2024-01-10 16:49:42 +0900

    ✨ feat: 라우터 설정 (#10)

--- src/pages/PublicRoute.tsx
+++ src/pages/PublicRoute.tsx
@@ -1,8 +1,11 @@
+import useUserInfo from "@/hooks/useUserInfo";
+
 interface PublicRouteProps {
 	LazyComponent: React.FC;
 }
 
 const PublicRoute: React.FC<PublicRouteProps> = ({ LazyComponent }) => {
+	useUserInfo();
 	return <LazyComponent />;
 };
 
