d4ed3e7528c0a5b53e4fcc34ae58d72895a6a299
Ahn taeuk <dotory0829@naver.com>
2024-02-09 22:18:27 +0900

    page-UserBIll 수정

--- src/pages/Page-UserBill.tsx
+++ src/pages/Page-UserBill.tsx
@@ -1,21 +1,34 @@
-import { getBillFromSupabase } from "@/api/supabase/playlistTableAccessApis";
+import {
+	deleteBill,
+	getBillFromSupabase,
+} from "@/api/supabase/playlistTableAccessApis";
+import { updateOwnTracklist } from "@/api/supabase/profilesTableAccessApis";
 import { Spinner } from "@/components";
 import BillBox_User from "@/components/bill/BillBox_User";
 import BillButtonListSection from "@/components/bill/BillButtonListSection";
+import BottomSheet from "@/components/common/BottomSheet";
+import ConfirmModal from "@/components/common/ConfirmModal";
 import Header from "@/components/common/Header";
 import SPINNER_TEXT from "@/constants/spinnerText";
+import useConfirm from "@/hooks/useConfirm";
+import { useModal } from "@/hooks/useModal";
 import { TrackList } from "@/types/recommendTypes";
+import Portal from "@/utils/portal";
 import usePlayNowStore from "@/zustand/playNowStore";
 import useRecommendStore from "@/zustand/recommendStore";
 import useUserStore from "@/zustand/userStore";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const UserBill = () => {
 	const userInfo = useUserStore((state) => state.userInfo);
 	const { id: playlistId, userid: userId } = useParams<string>();
+	const { openModal, modalType, closeModal } = useModal();
+	const { openConfirm, isShow } = useConfirm();
 	const currentTrack = usePlayNowStore((state) => state.currentTrack);
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
 	const resetRecommendStore = useRecommendStore(
 		(state) => state.resetRecommendStore,
 	);
@@ -30,11 +43,63 @@ const UserBill = () => {
 		enabled: !!playlistId,
 	});
 
+	//프로필에서 빌지삭제
+	const updateOwnTracklistMutation = useMutation({
+		mutationFn: updateOwnTracklist,
+		onSuccess() {
+			queryClient.invalidateQueries({
+				queryKey: ["profiles from supabase", userId],
+			});
+			closeModal();
+			navigate("/home");
+		},
+		onError(error) {
+			console.log(error);
+		},
+	});
+
+	//빌지테이블에서 빌지삭제
+	const deleteBillMutation = useMutation({
+		mutationFn: deleteBill,
+		onSuccess() {
+			updateOwnTracklistMutation.mutateAsync({
+				prevOwnTracklist: userInfo.own_tracklist,
+				billId: playlistId!,
+				userId: userId!,
+			});
+		},
+		onError(error) {
+			console.log(error);
+		},
+	});
+
+	const handleClickMoreButton = () => {
+		openModal("MYBILL_MORE");
+	};
+	const handleDeleteBill = () => {
+		deleteBillMutation.mutateAsync(playlistId as string);
+	};
+
+	// 수정하기 기능 추가 예정
+	const handleClickModalButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+		switch (e.currentTarget.innerText) {
+			case "삭제하기":
+				openConfirm("DELETE");
+				break;
+			default:
+				return;
+		}
+	};
+
 	if (isLoading) return <Spinner text={SPINNER_TEXT.BILL_TEXT} />;
 
 	return (
 		<>
-			<Header type="BILL" isNoneMore={!userInfo.id || userInfo.id !== userId} />
+			<Header
+				type="BILL"
+				onClickRightButton={handleClickMoreButton}
+				isNoneMore={!userInfo.id || userInfo.id !== userId}
+			/>
 			<div className="h-52 w-full bg-mainGreen pt-6">
 				<div className="mx-auto h-20 w-376 rounded-[10px] bg-[#282828]"></div>
 			</div>
@@ -43,6 +108,12 @@ const UserBill = () => {
 				data={data}
 				className={currentTrack ? "mb-180" : "mb-90"}
 			/>
+			<Portal>
+				{modalType === "MYBILL_MORE" && (
+					<BottomSheet onClick={handleClickModalButton} />
+				)}
+				{isShow && <ConfirmModal onConfirmClick={handleDeleteBill} />}
+			</Portal>
 		</>
 	);
 };
